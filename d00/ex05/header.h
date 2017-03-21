517cdc18e334aca9f84a7a526f0506a8dd56cc32
Dmitry <dz@SharkMacBookPro.local>
2017-03-21 12:27:43 -0700

    bigo for ex04

---
+++ d00/ex05/header.h
@@ -0,0 +1,24 @@
+#ifndef HEADER_H
+# define HEADER_H
+# include <stdio.h> //printf, scanf, ...
+# include <string.h> //memcpy, strlen, ...
+# include <unistd.h> //fork, write, sleep...
+# include <stdlib.h> //malloc, free, exit...
+# define MIN_LENGTH 4
+
+struct s_elem 
+{
+	char *word;
+	int  occurence;
+};
+
+struct s_list
+{
+	struct s_elem **elems;
+	int len;
+};
+
+char **mostUsedWords(char *book);
+char *readFile(void);
+
+#endif
---
+++ d00/ex05/mostUsedWords.c
@@ -0,0 +1,6 @@
+#include "header.h"
+
+char **mostUsedWords(char *book)
+{
+	
+}
