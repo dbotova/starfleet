f60776093e7db734b6861d81f4979ca4dfd26458
Darya Botova <dbotova@e1z2r4p30.42.us.org>
2017-03-22 15:32:55 -0700

    v1

--- d01/ex05/main.c
+++ d01/ex05/main.c
@@ -2,17 +2,68 @@
 
 int main(void)
 {
-	//struct s_tank *tank = initTank();
+	struct s_tank *tank = initTank();
 
-	/*-------------------
-	launch your test here
-	--------------------*/
-	//tankPush(tank, 10);
-	//tankPush(tank, 50);
+	tankPush(tank, 10);
+	tankPush(tank, 20);
+	tankPush(tank, 30);
+	// printf("\npoped: %d\n", tankPop(tank));
+	tankPush(tank, 40);
+	tankPush(tank, 50);
+	tankPush(tank, 60);
+	tankPush(tank, 70);
+	tankPush(tank, 80);
+	tankPush(tank, 90);
+	tankPush(tank, 100);
+	tankPush(tank, 100);
+	tankPush(tank, 100);
+	tankPush(tank, 100);
+	tankPush(tank, 100);
+	tankPush(tank, 100);
+	tankPush(tank, 15);
+	tankPush(tank, 25);
+	tankPush(tank, 35);
+	printf("\npoped: %d\n", tankPop(tank));
+	printf("\npoped: %d\n", tankPop(tank));
+	printf("\npoped: %d\n", tankPop(tank));
+	printf("\npoped: %d\n", tankPop(tank));
+	printf("\npoped: %d\n", tankPop(tank));
+	tankPush(tank, 100);
+	tankPush(tank, 100);
+	tankPush(tank, 100);
+	tankPush(tank, 100);
+	// printf("\npoped: %d\n", tankPop(tank));
+	// printf("\npoped: %d\n", tankPop(tank));
+	// printf("\npoped: %d\n", tankPop(tank));
+	// printf("\npoped: %d\n", tankPop(tank));
+	// printf("\npoped: %d\n", tankPop(tank));
+	// printf("\npoped: %d\n", tankPop(tank));
+	// printf("\npoped: %d\n", tankPop(tank));
+
+
+	// while (42)
+	// {
+	// 	char input[15];
+	// 	int energy = 0;
+
+	// 	printf("(''):");
+	// 	scanf("%s", input);
+
+	// 	if (strcmp(input, "exit") == 0)
+	// 		break ;
+	// 	if (strcmp(input, "pop") == 0)
+	// 		tankPop(tank);
+	// 	else if (strcmp(input, "push") == 0)
+	// 	{
+	// 		scanf("%d", &energy);
+	// 		tankPush(tank, energy);
+	// 	}
+	// 	else if (strcmp(input, "print") == 0)
+	// 		printTank(tank);
+	// 	else
+	// 		printf("Try again\n");
+	// }
+	
 	return (0);
 }
 
-
-
-// Function used for the test
-// Don't go further :)
--- d01/ex05/tank.c
+++ d01/ex05/tank.c
@@ -4,6 +4,7 @@ struct s_tank *initTank(void)
 {
 	struct s_tank *tank = malloc(sizeof(struct s_tank));
 	tank->stacks = malloc(sizeof(struct s_stack) * TANKS);
+	tank->stacks[0] = malloc(sizeof(struct s_stack));
 	tank->stacks[0]->elem = NULL;
 	tank->stacks[0]->sum = 0;
 	tank->n = 1;
@@ -19,56 +20,62 @@ void tankPush(struct s_tank *tank, int energy)
 	}
 	if (tank->stacks[tank->n - 1]->sum + energy > CAPACITY)
 	{
-		if (n > TANKS)
+		if (tank->n > TANKS)
 		{
 			printf("All reservoirs are full. Burn that energy!\n");
 			return ;
 		}
 		tank->n += 1;
+		tank->stacks[tank->n - 1] = malloc(sizeof(struct s_stack));
 	}
+	tank->stacks[tank->n - 1]->sum += energy;
+
 	struct s_elem *new = malloc(sizeof(struct s_elem));
 	new->energy = energy;
-	tank->stacks[n - 1]->sum += energy;
-	if (tank->stacks[tank->n - 1]->elem != NULL)
-		new->next = tank->stacks[tank->n - 1]->elem;
-	else
-		new->next = NULL;
+	new->next = tank->stacks[tank->n - 1]->elem;
 	tank->stacks[tank->n - 1]->elem = new;
+
 	printTank(tank);
 }
 
 int tankPop(struct s_tank *tank)
 {
-	if (tank->n == 1 && tank->stacks[tank->n - 1] == 0)
+	if (tank->n == 1 && tank->stacks[tank->n - 1]->sum == 0)
 	{
-		printf("We are out of energy, captain!%s\n");
+		printf("We are out of energy, captain!\n");
 		return(0);
 	}
 
-	int top = tank->stacks[tank->n - 1]->elem->energy;
-	tank->stacks[n - 1]->sum -= top;
-	if (tank->stacks[n - 1]->sum == 0)
+	struct s_elem *tmp = tank->stacks[tank->n - 1]->elem;
+	int top = tmp->energy;
+	tank->stacks[tank->n - 1]->sum -= top;
+	if (tank->stacks[tank->n - 1]->sum == 0 && tank->n > 1)
 		tank->n -= 1;
 	tank->stacks[tank->n - 1]->elem = tank->stacks[tank->n - 1]->elem->next;
+
 	printTank(tank);
+
+	free(tmp);
+	tmp = NULL;
+
 	return (top);
 }
 
 void printTank(struct s_tank *tank)
 {
-	if (!tank || !tank->stacks[tank->n - 1]->elem)
+	if (!tank)
 	{
 		printf("Oh no! We lost our tanks!\n");
 		return ;
 	}
-
+	
 	char *header = "------";
 	char *footer = "|    |";
 	
-	printf("normal");
+	printf("\nnormal");
 	for (int i = 1; i < tank->n; i++)
 	{
-		printf("\tnitro%d", i - 1);
+		printf("\tnitro%d", i);
 	}
 	printf("\n");
 
@@ -80,13 +87,14 @@ void printTank(struct s_tank *tank)
 
 	for (int j = 0; j < tank->n; j++)
 	{
-		printf("|%-4d|\t", tank->stacks[i]->sum);
+		printf("|%-4d|\t", tank->stacks[j]->sum);
 	}
 	printf("\n");
 
 	for (int j = 0; j < tank->n; j++)
 	{
-		printf("|%-4d|\t", tank->stacks[i]->sum);
+		printf("%s\t", footer);
 	}
-	printf("\n");
-}
+	printf("\n\n");
+}	
+
