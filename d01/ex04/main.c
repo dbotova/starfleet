3cf65262169ce2150a38a8795b63df9300604d07
Dmitry <dz@SharkMacBookPro.local>
2017-03-22 08:43:03 -0700

    ex04

--- d01/ex04/main.c
+++ d01/ex04/main.c
@@ -2,10 +2,18 @@
 
 int main(void)
 {
-
-	/*-------------------
-	launch your test here
-	--------------------*/
+	struct s_queue *queue = queueInit();
+	printf("is empty? %d\n", isEmpty(queue));
+	printf("peek %s\n", peek(queue));
+	enqueue(queue, "first");
+	enqueue(queue, "second");
+	printf("first: %s last: %s\n", queue->first->message, queue->last->message);
+	dequeue(queue);
+	printf("first: %s last: %s\n", queue->first->message, queue->last->message);
+	enqueue(queue, "hello");
+	dequeue(queue);
+	printf("is empty? %d\n", isEmpty(queue));
+	printf("peek %s\n", peek(queue));
 
 	return (0);
 }
--- d01/ex04/queue.c
+++ d01/ex04/queue.c
@@ -2,25 +2,48 @@
 
 struct s_queue *queueInit(void)
 {
-
+	struct s_queue *queue = malloc(sizeof(struct s_queue));
+	queue->first = NULL;
+	queue->last = NULL;
+	return (queue);
 }
 
 char *dequeue(struct s_queue *queue)
 {
+	char *top = NULL;
 
+	if (queue->first == NULL)
+		return (NULL);
+	top = queue->first->message;
+	struct s_node *tmp = queue->first;
+	free(tmp);
+	tmp = NULL;
+	queue->first = queue->first->next;
+	if (queue->first == NULL)
+		queue->last = NULL;
+	return (top);
 }
 
 void enqueue(struct s_queue *queue, char *message)
 {
-
+	struct s_node *new = malloc(sizeof(struct s_node));
+	new->message = strdup(message);
+	new->next = queue->first;
+	queue->first = new;
+	if (queue->last == NULL)
+		queue->last = new;
 }
 
 char *peek(struct s_queue *queue)
 {
-
+	if (queue->first == NULL)
+		return (NULL);
+	return (queue->first->message);
 }
 
 int isEmpty(struct s_queue *queue)
 {
-	
+	if (queue->first == NULL)
+		return (1);
+	return (0);
 }
