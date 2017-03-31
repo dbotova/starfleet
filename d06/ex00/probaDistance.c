e3e24c031469db0ecee8275ffe5bbdb5431979c5
dbotova <darya.botova@hotmail.com>
2017-03-30 20:45:16 -0700

    Add files via upload

--- d06/ex00/probaDistance.c
+++ d06/ex00/probaDistance.c
@@ -2,16 +2,21 @@
 
 double probaDistance(double dist, int *locations, int n)
 {
-	double probobility = 0;
+	if (n < 2)
+        return (0);
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			int dif = (locations[i] - locations[j] < 0) ? (locations[i] - locations[j]) * -1 : (locations[i] - locations[j]);
-			if (dif > dist)
-				probobility += 1 / (double)n;
-		}
-	}
-	return (probobility);
+    int left = 0;
+    int right = 0;
+    int count = 0;
+  
+    while (left < n)
+    {
+        if (right <= left)
+            right = left + 1;
+        while (right < n && locations[right] - locations[left] <= dist)
+            right++;
+        count += n - right;
+        left++;
+    }
+    return (((double)count * 2 / (double)(n * (n - 1))));
 }
